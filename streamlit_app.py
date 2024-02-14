ab76d6a65620a54c172778434cab6602090dd010
abdessamadelhajjami <149787830+abdessamadelhajjami@users.noreply.github.com>
2024-02-14 14:36:20 +0100

    Update streamlit_app.py
    Ajout de sous titre
    Ajout du Menu Breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit 
 streamlit.title('My parents New Healthy Diner')
+streamlit.subtitle('Breakfast Menu')
+streamlit.write('Omega 3 & Blueberry Oatmeal')
+streamlit.write('Kale, Spinach & Rcket Smoothie')
+streamlit.write('Hard-Boiled Free-Range Egg')
+
